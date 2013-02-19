f39e4ff150eced06c05ca6980e8079a45d09ecd3
Isaac Kauvar <isaackauvar@dn0a202a6d.sunet>
2013-02-18 20:07:55 -0800

    cleaned up the interruptTest which demonstrates how to use interrupts on arduino

--- interruptTest/interruptTest.ino
+++ interruptTest/interruptTest.ino
@@ -1,10 +1,7 @@
 /**************************************************************
   File:      interruptTest.pde 
-  Contents:  This module digitally determines the frequency of a periodic input
-             signal. It does so by attaching an interrupt function to the input pin
-             that stores the time of each rising edge of the input signal,
-             and then calculating the frequency based on these times. 
-             Based on work by Scott Cauble.
+  Contents:  This program demonstrates how to use the interrupt feature
+            of an arduino. Notice that pin2 = interrupt pin #0
 
   Notes:    Target: Arduino UNO R1 & R2
             Arduino IDE version: 0022
@@ -20,50 +17,27 @@
 
  
 /*---------------- Module Defines ---------------------------*/
-#define numPulsesToAverage 3 //length of buffer that stores clock times from the interrupt function
 #define IRFREQreadpin 2 //interrupt pin #0, digital pin #2
-#define MAXpulsebufferlength 200000 //in microseconds?. max time between first and last entry in pulseBuffer.
-                                      //if the span of the pulseBuffer is greater than this than the beacon
-                                      //was likely turned off during detection
-#define MAXpulseactualtime 1000000 //in microseconds. Ensure that all pulseBuffer values have occured within
-                                     // the last second.
+            
+volatile int state = HIGH;           
                                      
-//TO IDENTIFY BEACON TYPE
-#define SEQUESTER 1
-#define ENEMY 2
-#define NEITHER 0
-
-/* REQUIRED module-level variables */
-unsigned long PULSEFREQ_pulsebuffer[numPulsesToAverage]; //the pulseBuffer array of interrupt times
-char PULSEFREQ_bufferindex = 0; //the index of the oldest value in the array
-boolean PULSEFREQ_scanning = false; // 'true' if the interrupt is enabled
-
-/* Function prototypes */
-void PULSEFREQ_startscanning(void);
-void PULSEFREQ_stopscanning(void);
-void PULSEFREQ_detected(void);
-unsigned long PULSEFREQ_period(void);
-int PULSEFREQ_frequency(void);
-int PULSEFREQ_beacontype(int pulsefreq);
-
-
 void setup()
 {
   pinMode(IRFREQreadpin, INPUT);
   pinMode(5, OUTPUT);
   pinMode(4, OUTPUT);
-  attachInterrupt(IRFREQreadpin, respondToInterrupt, RISING);
+  attachInterrupt(IRFREQreadpin-2, respondToInterrupt, RISING);
+  tone(5, 850);
 }
 
 void loop()
 {
-  analogWrite(5, 123);
-  digitalWrite(4, LOW);
+  digitalWrite(4, state);
 }
 
 void respondToInterrupt()
 {
-  digitalWrite(4, HIGH);
+  state = !state;
 }
   
   
