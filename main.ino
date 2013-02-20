4f3d10d8e549db4464b23e864be516076901119d
Evan Clark <evan.bock.clark@gmail.com>
2013-02-19 22:31:12 -0800

    Added template files and initial main files

---
+++ main.ino
@@ -0,0 +1,37 @@
+#pragma once
+
+/***************************************************************
+ Filename:         
+ Contents:
+ 
+ Notes:    Target: Arduino UNO R1 & R2
+ Arduino IDE version: 0022
+ 
+ History:
+ when      who  what/why
+ 
+ **************************************************************/
+
+/*---------------- Includes ---------------------------------*/
+#include <Timers.h>
+
+
+/*---------------- Module Defines ---------------------------*/
+#define IRFREQreadpin 2 //interrupt pin #0, digital pin #2
+
+
+/*---------------- Module Types ---------------------------*/
+
+/*---------------- Module Level Variables ---------------------------*/
+
+/*---------------- Module Functions ---------------------------*/
+
+/*---------------- Module Code ---------------------------*/
+
+void setup() {
+    Serial.begin(9600);
+}
+
+void loop() {
+    
+}
---
+++ template.h
@@ -0,0 +1,26 @@
+#pragma once
+
+//#define TESTING //comment this out to use test harness
+
+/**************************************************************
+ Filename:
+ Contents:
+ 
+ Notes:    Target: Arduino UNO R1 & R2
+ Arduino IDE version: 0022
+ 
+ History:
+ when      who  what/why
+ 
+ **************************************************************/
+
+/*---------------- Includes ---------------------------------*/
+
+/*---------------- Module Level Defines ---------------------------------*/
+
+/*---------------- Module Level Variables ---------------------------*/
+
+/*---------------- Module Public Function Prototypes ---------------------------*/
+
+/*---------------- Module Private Function Prototypes ---------------------------*/
+
